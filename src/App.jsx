8abdf94c6b4ab1be7ee3d58598ee61c8baf46947
Frank Andrade <fr4nky@durazno.io>
2023-01-20 16:16:29 -0600

    modify flow

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CoordinadorRegister from './routes/coordinator-register';
 import DirectorRegister from './routes/director-register';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
+import Home from './components/Home';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
 
@@ -12,7 +13,14 @@ const App = () => {
   // Use the value of authStatus to decide which page to render
   return (
     <Routes>
-      <Route path='/' element={<Pregister />} />
+      <Route
+        path='/'
+        element={
+          <Authenticator.Provider>
+            <Home />
+          </Authenticator.Provider>
+        }
+      />
       <Route exact path='/pre-registro' element={<Pregister />} />
       <Route
         exact
---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+import { useAuthenticator } from '@aws-amplify/ui-react';
+
+const Profile = () => {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+  console.log(user);
+  return (
+    <div className='container'>
+      <div className='mt-24 border border-solid border-red-50'>
+        <p className='text-pink-900 text-lg font-bold'>Hola {user.attributes.name} lol</p>
+        <p className='text-pink-800 text-sm font-bold mt-2'>
+          Da click en el boton Descargar para obtener los registros de la gente
+        </p>
+      </div>
+      <div className='mt-8 flex flex-col '>
+        <a
+          href='#_'
+          className='relative rounded px-5 py-2.5 overflow-hidden group bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300'
+        >
+          <span className='absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease'></span>
+          <span className='relative'>Descargar Registros</span>
+        </a>
+        <a
+          onClick={signOut}
+          className='mt-4 relative rounded px-5 py-2.5 overflow-hidden group bg-pink-800 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300'
+        >
+          <span className='absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease'></span>
+          <span className='relative'>Salir</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
