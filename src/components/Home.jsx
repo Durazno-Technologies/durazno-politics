294a60bf2d9c1eceec3d2cff7fa8346d32d11b7f
Frank Andrade <fr4nky@durazno.io>
2023-01-22 21:41:35 -0600

    changed styles home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,23 +20,25 @@ const Home = () => {
   }, [authStatus]);
 
   return (
-    <div className='container mt-8'>
-      <h1 className='mt-12 text-pink-700 text-lg font-bold'>Selecciona tu perfil</h1>
-      <div className='mt-8'>
-        <button
-          type='button'
-          className='inline-block w-full px-6 py-4 bg-pink-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-pink-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out max-w-xs'
-          onClick={() => navigate('/dirigente')}
-        >
-          Dirigente
-        </button>
-        <button
-          type='button'
-          className='mt-4 w-full inline-block px-6 py-4 bg-pink-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-pink-700 hover:shadow-lg focus:bg-pink-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out max-w-xs'
-          onClick={() => navigate('/coordinador')}
-        >
-          Coordinador
-        </button>
+    <div className='container mt-8 flex justify-center'>
+      <div>
+        <h1 className='mt-12 text-pink-700 text-lg font-bold'>Selecciona tu perfil</h1>
+        <div className='mt-8 flex flex-col'>
+          <button
+            type='button'
+            className='inline-block w-full px-6 py-4 bg-pink-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-pink-700 hover:shadow-lg focus:bg-pink-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out max-w-xs'
+            onClick={() => navigate('/dirigente')}
+          >
+            Dirigente
+          </button>
+          <button
+            type='button'
+            className='mt-4 w-full inline-block px-6 py-4 bg-pink-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-pink-700 hover:shadow-lg focus:bg-pink-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out max-w-xs'
+            onClick={() => navigate('/coordinador')}
+          >
+            Coordinador
+          </button>
+        </div>
       </div>
     </div>
   );
